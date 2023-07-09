e5e7db3bb294602f742a8c0f3f2637c1358d4777
Anton_python <ogarkov.anton91@yandex.ru>
2023-07-09 13:47:07 +0300

    HW_2

---
+++ HW_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int maxi = n1;
+if (n2 > maxi)
+maxi = n2;
+if (n3 > maxi)
+maxi = n3;
+Console.WriteLine(maxi);
