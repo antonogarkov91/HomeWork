63b8a694dd435e399b4b42dc7236dd4e40b63b2e
Anton_python <ogarkov.anton91@yandex.ru>
2023-07-09 13:56:35 +0300

    HW_3

---
+++ HW_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число
+// и выдает, является ли число четным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % 2 == 0)
+Console.WriteLine("y");
+else
+Console.WriteLine("no");
+
