7463e25edbcf9e59fc1060c6e654c4c8c828dadc
Anton_python <ogarkov.anton91@yandex.ru>
2023-07-09 14:06:55 +0300

    HW_4

---
+++ HW_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= n1; i += 2)
+Console.WriteLine(i);
